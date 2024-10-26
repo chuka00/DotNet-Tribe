0125a9af245672a71bcfe45131a66a803431fdec
KCM <egwunwokekc@gmail.com>
2024-10-26 23:15:44 +0100

    fix: ⚒️fixed responsiveness and other minor ui fixes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Logo from "../Assets/Trybe-.png";
 import { BsTwitter } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
@@ -21,23 +20,23 @@ const Footer = () => {
       <div className="footer-section-two">
         <div className="footer-section-columns">
           <span>Meet the Tribe Leaders</span>
-        <div className="footer-links">
-        <div className="tribe-icons">
-          <span><a href="https://www.linkedin.com/in/kenechukwu-egwunwoke/" style={{color: 'white'}}><SiLinkedin />KCM</a></span>
-          <span><a href="https://www.linkedin.com/in/victor-ogbodo/" style={{color: 'white'}}><SiLinkedin />Victor Ogbodo</a></span>
-          <span><a href="https://www.linkedin.com/in/davidukpoju/" style={{color: 'white'}}><SiLinkedin />David Ukpoju</a></span>
+          <div className="footer-links">
+            <div className="tribe-icons">
+              <span><a href="https://www.linkedin.com/in/kenechukwu-egwunwoke/" style={{ color: 'white' }}><SiLinkedin />KCM</a></span>
+              <span><a href="https://www.linkedin.com/in/victor-ogbodo/" style={{ color: 'white' }}><SiLinkedin />Victor Ogbodo</a></span>
+              <span><a href="https://www.linkedin.com/in/davidukpoju/" style={{ color: 'white' }}><SiLinkedin />David Ukpoju</a></span>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
-        <div className="footer-section-three">
+      </div>
+      <div className="footer-section-three">
         <div className="footer-section-columns">
-        <span>dotnettribe@gmail.com</span>
+          <span>dotnettribe@gmail.com</span>
           <span>Terms & Conditions</span>
           <span>Privacy Policy</span>
           <span>Copyright @2024 DotnetTribe</span>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
